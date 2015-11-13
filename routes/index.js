6f142ca1d6badb6b7b0ab1054fc5d8a94dabd321
Ryan Bird <ryan01bird@gmail.com>
2015-11-13 11:38:32 -0700

    modified object

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,45 @@
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
-var conString = "postgres://@localhost/memoriesapp";
+// var conString = "postgres://@localhost/memoriesapp";
+var conString = process.env.DATABASE_URL
+
+router.get('/api/v1/memories', function(req, res, next) {
+  console.log('...conected')
+  pg.connect(conString, function(err, client, done) {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    client.query('SELECT * FROM memories', function(err,  result) {
+      
+      var modifiedResult = {
+        links: {},
+        data: []
+      }
+      
+      for(var i=0; i<result.rows.length; i++){
+        var fabulousObject = {
+          type: 'memory',
+          id: result.rows[i].id,
+          attributes: {
+            old_days: result.rows[i].old_days,
+            these_days:result.rows[i].these_days,
+            year:result.rows[i].year
+          },
+          links: {}
+        }
+        modifiedResult.data.push(fabulousObject)
+      }
+      
+      done();
+
+      if (err) {
+        return console.error('error running query', err);
+      }
+      res.send(modifiedResult);
+    });
+  });
+});
 
 
 /* GET users listing. */
@@ -13,13 +51,14 @@ router.post('/api/v1/memories', function(req, res, next) {
     }
     client.query('INSERT into memories(old_days, these_days, year) values($1, $2, $3)', [req.body.data.attributes.old_days, req.body.data.attributes.these_days, req.body.data.attributes.year], function(err, result) {
       done();
-      res.render('index', {});
 
       if (err) {
         return console.error('error running query', err);
       }
+      res.send(result);
     });
   });
 });
+
 
 module.exports = router;
